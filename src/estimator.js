f833c426fc09f34bde37f2895e4102f471e534bf
matsinvasion <matkmusasizi@gmail.com>
2020-04-14 12:32:11 +0300

    fix eslint issues

--- src/estimator.js
+++ src/estimator.js
@@ -1,43 +1,43 @@
 import * as helperFunctions from './helper';
 
 const covid19ImpactEstimator = (data) => {
-    //  Estimator
-    const estimator = {};
-    estimator.data = data;
-    estimator.impact = {};
-    estimator.severeImpact = {};
-    estimator.impact.currentlyInfected = data.reportedCases * 10;
-    estimator.impact.infectionsByRequestedTime = estimator.impact.currentlyInfected
-        * helperFunctions.projectedInfections(data);
-    estimator.impact.severeCasesByRequestedTime = Math.trunc(0.15 * estimator.impact
-      .infectionsByRequestedTime);
-    estimator.impact.hospitalBedsByRequestedTime = Math.trunc((0.35 * data.totalHospitalBeds)
-          - estimator.impact.severeCasesByRequestedTime);
-    estimator.impact.casesForICUByRequestedTime = Math.trunc(0.05 * estimator
-      .impact
-      .infectionsByRequestedTime);
-    estimator.impact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * estimator.impact
-      .infectionsByRequestedTime);
-    estimator.impact.dollarsInFlight = Math.trunc((estimator.impact.infectionsByRequestedTime * data
-      .region.avgDailyIncomePopulation * data
-      .region.avgDailyIncomeInUSD) / helperFunctions.timeToElapse(data));
-    estimator.severeImpact.currentlyInfected = data.reportedCases * 50;
-    estimator.severeImpact.infectionsByRequestedTime = estimator.severeImpact.currentlyInfected
-         * helperFunctions.projectedInfections(data);
-    estimator.severeImpact.severeCasesByRequestedTime = Math.trunc(0.15 * estimator.severeImpact
-      .infectionsByRequestedTime);
-    estimator.severeImpact.hospitalBedsByRequestedTime = Math.trunc((0.35 * data.totalHospitalBeds)
-          - estimator.severeImpact.severeCasesByRequestedTime);
-    estimator.severeImpact.casesForICUByRequestedTime = Math.trunc(0.05 * estimator.severeImpact
-      .infectionsByRequestedTime);
-    estimator.severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * estimator
-      .severeImpact
-      .infectionsByRequestedTime);
-    estimator.severeImpact.dollarsInFlight = Math.trunc((estimator.severeImpact
-      .infectionsByRequestedTime * data
-      .region.avgDailyIncomePopulation * data
-      .region.avgDailyIncomeInUSD) / helperFunctions.timeToElapse(data));
-    return estimator;
-  };
-  
-  export default covid19ImpactEstimator;
+  const estimator = {};
+  estimator.data = data;
+  estimator.impact = {};
+  estimator.severeImpact = {};
+  estimator.impact.currentlyInfected = data.reportedCases * 10;
+  estimator.impact.infectionsByRequestedTime = estimator.impact.currentlyInfected
+  * helperFunctions.projectedInfections(data);
+  estimator.impact.severeCasesByRequestedTime = Math.trunc(0.15 * estimator.impact
+    .infectionsByRequestedTime);
+  estimator.impact.hospitalBedsByRequestedTime = Math.trunc((0.35 * data.totalHospitalBeds)
+        - estimator.impact.severeCasesByRequestedTime);
+  estimator.impact.casesForICUByRequestedTime = Math.trunc(0.05 * estimator
+    .impact
+    .infectionsByRequestedTime);
+  estimator.impact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * estimator.impact
+    .infectionsByRequestedTime);
+  estimator.impact.dollarsInFlight = Math.trunc((estimator.impact.infectionsByRequestedTime * data
+    .region.avgDailyIncomePopulation * data
+    .region.avgDailyIncomeInUSD) / helperFunctions.timeToElapse(data));
+  estimator.severeImpact.currentlyInfected = data.reportedCases * 50;
+  estimator.severeImpact.infectionsByRequestedTime = estimator.severeImpact.currentlyInfected
+       * helperFunctions.projectedInfections(data);
+  estimator.severeImpact.severeCasesByRequestedTime = Math.trunc(0.15 * estimator.severeImpact
+    .infectionsByRequestedTime);
+  estimator.severeImpact.hospitalBedsByRequestedTime = Math.trunc((0.35 * data.totalHospitalBeds)
+        - estimator.severeImpact.severeCasesByRequestedTime);
+  estimator.severeImpact.casesForICUByRequestedTime = Math.trunc(0.05 * estimator.severeImpact
+    .infectionsByRequestedTime);
+  estimator.severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * estimator
+    .severeImpact
+    .infectionsByRequestedTime);
+  estimator.severeImpact.dollarsInFlight = Math.trunc((estimator.severeImpact
+    .infectionsByRequestedTime * data
+    .region.avgDailyIncomePopulation * data
+    .region.avgDailyIncomeInUSD) / helperFunctions.timeToElapse(data));
+  return estimator;
+};
+
+
+export default covid19ImpactEstimator;
